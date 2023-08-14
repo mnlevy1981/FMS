f3e54c385925a928d8e9de7798bfe38986fb1180
alperaltuntas <alperaltuntas@gmail.com>
2023-08-14 14:14:35 -0600

    apply CESM-specific constants module changes.

--- constants/fmsconstants.F90
+++ constants/fmsconstants.F90
@@ -55,6 +55,35 @@ module FMSconstants
 
   use platform_mod, only: r4_kind, r8_kind
 
+#define CESM_CONSTANTS
+#if defined(CESM_CONSTANTS)
+use shr_kind_mod,   only : R8 => shr_kind_r8
+use shr_const_mod,  only : &
+     SHR_CONST_PSTD,                      & ! standard pressure ~ pascals
+     PI              => SHR_CONST_PI,     &
+     PI_8            => SHR_CONST_PI,     &
+     SECONDS_PER_DAY => SHR_CONST_CDAY,   &
+     OMEGA           => SHR_CONST_OMEGA,  &
+     RADIUS          => SHR_CONST_REARTH, &
+     GRAV            => SHR_CONST_G,      &
+     STEFAN          => SHR_CONST_STEBOL, & ! Stefan-Boltzmann constant ~ W/m^2/K^4
+     AVOGNO          => SHR_CONST_AVOGAD, & ! Avogadro's number ~ molecules/kmole
+     WTMAIR          => SHR_CONST_MWDAIR, & ! molecular weight dry air ~ kg/kmole
+     WTMH2O          => SHR_CONST_MWWV,   & ! molecular weight water vapor
+     RDGAS           => SHR_CONST_RDAIR,  & ! Dry air gas constant     ~ J/K/kg
+     RVGAS           => SHR_CONST_RWV,    & ! Water vapor gas constant ~ J/K/kg
+     VONKARM         => SHR_CONST_KARMAN, & ! Von Karman constant
+     TFREEZE         => SHR_CONST_TKFRZ,  & ! freezing T of fresh water          ~ K
+     RHOAIR          => SHR_CONST_RHODAIR,& ! density of dry air at STP  ~ kg/m^3
+     DENS_H2O        => SHR_CONST_RHOFW,  & ! density of fresh water     ~ kg/m^3
+     RHO0            => SHR_CONST_RHOSW,  & ! density of sea water       ~ kg/m^3
+     CP_AIR          => SHR_CONST_CPDAIR, & ! specific heat of dry air   ~ J/kg/K
+     CP_VAPOR        => SHR_CONST_CPWV,   & ! specific heat of water vap ~ J/kg/K
+     CP_OCEAN        => SHR_CONST_CPSW,   &
+     HLF             => SHR_CONST_LATICE, & ! latent heat of fusion      ~ J/kg
+     HLV             => SHR_CONST_LATVAP    ! latent heat of evaporation ~ J/kg
+#endif
+
   !--- default scoping
   implicit none
 
@@ -64,6 +93,10 @@ module FMSconstants
 #define RKIND sizeof(dum)
 
 !--- set a default for the FMSConstants
+#if defined(CESM_CONSTANTS)
+#include <cesm_constants.fh>
+#else
+
 #if !defined(GFDL_CONSTANTS) && !defined(GFS_CONSTANTS) && !defined(GEOS_CONSTANTS)
 #define GFDL_CONSTANTS
 #endif
@@ -81,6 +114,8 @@ module FMSconstants
 #else
 #error FATAL FMSConstants error -  multiple constants macros are defined for FMS
 #endif
+
+#endif ! ifdef CESM_CONSTANTS
 
   !--- public interfaces
   public :: FMSConstants_init
